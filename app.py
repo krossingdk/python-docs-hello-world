42b98b7f5ee7728a4bbceab68e27e0a5623f56fd
krossingdk <40040481+krossingdk@users.noreply.github.com>
2021-06-27 22:16:10 +0200

    Update app.py

--- app.py
+++ app.py
@@ -7,17 +7,11 @@ import pickle
 from flask import Flask, request, render_template, redirect, url_for
 import json
 
-
-
-project_root = os.path.dirname(os.path.realpath('__file__'))
-template_path = os.path.join(project_root, 'app/templates')
-static_path = os.path.join(project_root, 'app/static')
-app = Flask(__name__, template_folder=template_path, static_folder=static_path)
+app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    #exec(open('schedule.py').read())
     return 'On the way to automatic scheduling'
 
 
