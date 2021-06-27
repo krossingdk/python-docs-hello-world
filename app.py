bcb57a980d4fc72f960db6d3312d1c24736322e4
krossingdk <40040481+krossingdk@users.noreply.github.com>
2021-06-27 22:29:47 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,6 @@
-
 from flask import Flask
 app = Flask(__name__)
 
-
-@app.route('/')
-def index():
-    return 'On the way to automatic scheduling'
-
-
-
+@app.route("/")
+def hello():
+    return "Hello, World!"
