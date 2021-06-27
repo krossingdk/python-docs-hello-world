16ab9f8ba8d78998860952f3474e84ca7a5978e7
krossingdk <40040481+krossingdk@users.noreply.github.com>
2021-06-27 22:46:51 +0200

    Update app.py

--- app.py
+++ app.py
@@ -9,3 +9,20 @@ import json
 @app.route("/")
 def hello():
     return "Hello, Kasper Rossing!"
+
+@app.route('/json', methods=['POST'])
+def json():
+    
+    import json
+    data={}
+    with open('brain.json', 'w') as outfile:
+            json.dump(data, outfile)
+    request_data = request.get_json()
+    with open('data.json', 'w') as file:
+        json.dump(request_data ,file)
+    import os
+    os.system("python brain2.py")
+    with open('brain.json') as json_file:
+        data = json.load(json_file)
+    #return request_data
+    return data
